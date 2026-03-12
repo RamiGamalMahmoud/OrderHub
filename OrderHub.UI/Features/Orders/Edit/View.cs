04e22bfc95e3911c8077361fb4187c83f7fd7d92
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-03-12 18:48:42 +0200

    Add Orders Edit View and ViewModel
    Introduce skeleton classes for the Orders Edit feature. Adds OrderHub.UI.Features.Orders.Edit.View (inherits Editor.View) and ViewModel (inherits Editor.ViewModel) with DI for IMediator, IDialogService and IMessenger. ViewModel sets localized Title and ActionName (Arabic) and leaves Save() unimplemented (throws NotImplementedException) to be implemented later.

---
+++ OrderHub.UI/Features/Orders/Edit/View.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderHub.UI.Features.Orders.Edit;
+
+internal class View : Editor.View
+{
+    public View(ViewModel viewModel) : base(viewModel)
+    {
+    }
+}
---
+++ OrderHub.UI/Features/Orders/Edit/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using MediatR;
+using OrderHub.UI.Interfaces;
+using System.Threading.Tasks;
+
+namespace OrderHub.UI.Features.Orders.Edit;
+
+internal class ViewModel : Editor.ViewModel
+{
+    public ViewModel(IMediator mediator, IDialogService dialogService, IMessenger messenger) : base(mediator, dialogService, messenger)
+    {
+    }
+
+    public override string ActionName => "حفظ التعديلات";
+    public override string Title => "تعديل طلب";
+
+    protected override Task Save()
+    {
+        throw new System.NotImplementedException();
+    }
+}
