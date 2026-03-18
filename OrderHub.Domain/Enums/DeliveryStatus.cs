ed919f9e3adc2436cb5eca7a72784a875f3eda30
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-03-18 21:10:43 +0200

    Add DeliveryStatus enum with Arabic descriptions

---
+++ OrderHub.Domain/Enums/DeliveryStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace OrderHub.Domain.Enums;
+
+public enum DeliveryStatus
+{
+    [Description("قيد الانتظار")]
+    Pending,
+
+    [Description("قيد التنفيذ")]
+    InProgress,
+
+    [Description("مكتمل")]
+    Completed
+}
