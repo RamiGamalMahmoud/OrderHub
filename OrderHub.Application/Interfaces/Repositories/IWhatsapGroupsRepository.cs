8cf5852b1b8b49dd131bd751c182715559a74115
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-03-26 14:12:25 +0200

    chore: add GetWhatsappGroupInfos and GetWhatsappGroupLists to WhatsappRepository

--- OrderHub.Application/Interfaces/Repositories/IWhatsapGroupsRepository.cs
+++ OrderHub.Application/Interfaces/Repositories/IWhatsapGroupsRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace OrderHub.Application.Interfaces.Repositories;
+﻿using OrderHub.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrderHub.Application.Interfaces.Repositories;
 
 public interface IWhatsapGroupsRepository
 {
+    Task<IEnumerable<WhatsappGroupDtos.WhatsappGroupInfoDto>> GetWhatsappGroupInfos();
+    Task<IEnumerable<WhatsappGroupDtos.WhatsappGroupListDto>> GetWhatsappGroupLists();
 }
--- OrderHub.Infrastructure/Repositories/WhatsapGroupsRepository.cs
+++ OrderHub.Infrastructure/Repositories/WhatsapGroupsRepository.cs
@@ -1,7 +1,28 @@
-﻿using OrderHub.Application.Interfaces.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using OrderHub.Application.Interfaces.Repositories;
+using OrderHub.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static OrderHub.Application.DTOs.WhatsappGroupDtos;
 
 namespace OrderHub.Infrastructure.Repositories;
 
-internal class WhatsapGroupsRepository : IWhatsapGroupsRepository
+internal class WhatsapGroupsRepository(AppDbContextFactory appDbContextFactory) : IWhatsapGroupsRepository
 {
+    public async Task<IEnumerable<WhatsappGroupListDto>> GetWhatsappGroupLists()
+    {
+        using AppDbContext appDbContext = appDbContextFactory.CreateDbContext();
+        return await appDbContext.Set<WhatsappGroup>()
+            .Select(w => new WhatsappGroupListDto(w.Id, w.GroupName, w.GroupType))
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<WhatsappGroupInfoDto>> GetWhatsappGroupInfos()
+    {
+        using AppDbContext appDbContext = appDbContextFactory.CreateDbContext();
+        return await appDbContext.Set<WhatsappGroup>()
+            .Select(w => new WhatsappGroupInfoDto(w.Id, w.GroupName, w.GroupType))
+            .ToListAsync();
+    }
 }
