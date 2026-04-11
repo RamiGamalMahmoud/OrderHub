b1ff749a181ba7287742b1e13aa1e48d87f6eec8
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-04-11 16:19:04 +0200

    chore: move order items grid to new component

---
+++ OrderHub.UI/Features/Orders/Editor/Components/OrderItemsComponent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderHub.UI.Features.Orders.Editor.Components
+{
+    /// <summary>
+    /// Interaction logic for OrderItemsComponent.xaml
+    /// </summary>
+    public partial class OrderItemsComponent : UserControl
+    {
+        public OrderItemsComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
