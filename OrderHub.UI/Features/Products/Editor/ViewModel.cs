2e846a60da4cc5296a4acb9ee8f961fab42d7c6a
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-07-25 18:55:04 +0300

    feat(products): redesign product properties section

--- OrderHub.UI/Features/Products/Editor/ViewModel.cs
+++ OrderHub.UI/Features/Products/Editor/ViewModel.cs
@@ -2,106 +2,145 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using MediatR;
+using OrderHub.Domain.Enums;
 using OrderHub.UI.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using static OrderHub.Application.DTOs.CommonDtos;
 
-namespace OrderHub.UI.Features.Products.Editor
+namespace OrderHub.UI.Features.Products.Editor;
+
+public abstract partial class ViewModel : EditorViewModelBase
 {
-    public abstract partial class ViewModel : EditorViewModelBase
+    protected readonly IMediator _mediator;
+    protected readonly IMessenger _messenger;
+
+    protected ViewModel(IMediator mediator, IMessenger messenger)
     {
-        protected readonly IMediator _mediator;
-        protected readonly IMessenger _messenger;
-
-        protected ViewModel(IMediator mediator, IMessenger messenger)
-        {
-            _mediator = mediator;
-            _messenger = messenger;
-            _notifyPropertiesNames = [nameof(Name), nameof(SelectedCategory), nameof(PriceText), nameof(Code)];
-            ValidateAllProperties();
-            RegisterMessages();
-            CategorySelection = new CategorySelection(mediator);
-        }
-
-        private void RegisterMessages()
-        {
-            _messenger.Register<Application.Messages.Categories.CategoryCreatedMessage>(this, async (_, _) => await RefreshCategoriesAsync());
-            _messenger.Register<Application.Messages.Categories.CategoryUpdatedMessage>(this, async (_, _) => await RefreshCategoriesAsync());
-            _messenger.Register<Application.Messages.Suppliers.SupplierCreatedMessage>(this, async (_, _) => await RefreshSuppliersAsync());
-            _messenger.Register<Application.Messages.Suppliers.SupplierUpdatedMessage>(this, async (_, _) => await RefreshSuppliersAsync());
-        }
-
-        public virtual async Task LoadDataAsync()
-        {
-            await RefreshCategoriesAsync();
-            await RefreshSuppliersAsync();
-            await CategorySelection.LoadRootCategoriesAsync();
-        }
-
-        public CategorySelection CategorySelection { get; }
-        public bool IsFullPathVisible => SelectedCategory is not null;
-
-        private async Task RefreshCategoriesAsync()
-            => Categories = await _mediator.Send(new Application.Queries.CommonQueries.GetCategoriesInfoQuery());
-
-        private async Task RefreshSuppliersAsync()
-        {
-            IEnumerable<SupplierInfoDto> suppliers = await _mediator.Send(new Application.Queries.CommonQueries.GetSuppliersInfoQuery());
-            Suppliers = suppliers.Select(s => new SelectableObject<SupplierInfoDto>(s)).ToList();
-        }
-
-        [RelayCommand]
-        private void SelectSupplier() => HasChanges = true;
-
-        partial void OnPriceTextChanged(string oldValue, string newValue)
-        {
-            if(decimal.TryParse(newValue, out decimal price))
-            {
-                Price = price;
-            }
-        }
-
-        [ObservableProperty]
-        [Required(ErrorMessage = "اسم المنتج مطلوب")]
-        [MinLength(2, ErrorMessage = "الاسم يجب أن يكون أكثر من 2 حرف")]
-        [NotifyDataErrorInfo]
-        [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
-        private string _name;
-
-        [ObservableProperty]
-        [Required(ErrorMessage = "القسم مطلوب")]
-        [NotifyDataErrorInfo]
-        [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
-        private CategoryInfoDto _selectedCategory;
-
-        partial void OnSelectedCategoryChanged(CategoryInfoDto oldValue, CategoryInfoDto newValue)
-        {
-            CategorySelection.SelectedCategory = newValue;
-        }
-
-        [ObservableProperty]
-        [Required(ErrorMessage = "السعر مطلوب")]
-        [RegularExpression(@"^\d+([.,]\d{0,2})?$", ErrorMessage = "السعر غير صالح")]
-        [NotifyDataErrorInfo]
-        [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
-        private string _priceText;
-
-        [ObservableProperty]
-        [Required(ErrorMessage = "كود المنتج مطلوب")]
-        [RegularExpression(@"^\d{3}-\d{2}-\d{2}", ErrorMessage = "كود المنتج يتكون من الصورة 000-00-00")]
-        [NotifyDataErrorInfo]
-        [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
-        private string _code;
-
-        public decimal Price { get; private set; }
-
-        [ObservableProperty]
-        private IEnumerable<SelectableObject<SupplierInfoDto>> _suppliers;
-
-        [ObservableProperty]
-        private IEnumerable<CategoryInfoDto> _categories;
+        _mediator = mediator;
+        _messenger = messenger;
+        _notifyPropertiesNames = [nameof(Name), nameof(SelectedCategory), nameof(PriceText), nameof(Code)];
+        ValidateAllProperties();
+        RegisterMessages();
+        CategorySelection = new CategorySelection(mediator);
     }
+
+    private void RegisterMessages()
+    {
+        _messenger.Register<Application.Messages.Categories.CategoryCreatedMessage>(this, async (_, _) => await RefreshCategoriesAsync());
+        _messenger.Register<Application.Messages.Categories.CategoryUpdatedMessage>(this, async (_, _) => await RefreshCategoriesAsync());
+        _messenger.Register<Application.Messages.Suppliers.SupplierCreatedMessage>(this, async (_, _) => await RefreshSuppliersAsync());
+        _messenger.Register<Application.Messages.Suppliers.SupplierUpdatedMessage>(this, async (_, _) => await RefreshSuppliersAsync());
+    }
+
+    public virtual async Task LoadDataAsync()
+    {
+        await RefreshCategoriesAsync();
+        await RefreshSuppliersAsync();
+        await CategorySelection.LoadRootCategoriesAsync();
+        Properties = (await _mediator.Send(new Application.Features.Setup.Properties.GetAll.GetPropertiesQuery()))
+            .Select(x => new PropertyItem()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                PropertyType = x.Type.ToString()
+            });
+
+        PropertyRequirements = Enum.GetValues<PropertyRequirement>()
+        .Select(x => new EnumItem<PropertyRequirement>(x, x.GetDescription()));
+        OnPropertyChanged(nameof(Properties));
+    }
+
+    public CategorySelection CategorySelection { get; }
+    public bool IsFullPathVisible => SelectedCategory is not null;
+
+    private async Task RefreshCategoriesAsync()
+        => Categories = await _mediator.Send(new Application.Queries.CommonQueries.GetCategoriesInfoQuery());
+
+    private async Task RefreshSuppliersAsync()
+    {
+        IEnumerable<SupplierInfoDto> suppliers = await _mediator.Send(new Application.Queries.CommonQueries.GetSuppliersInfoQuery());
+        Suppliers = suppliers.Select(s => new SelectableObject<SupplierInfoDto>(s)).ToList();
+    }
+
+    [RelayCommand]
+    private void SelectSupplier() => HasChanges = true;
+
+    partial void OnPriceTextChanged(string oldValue, string newValue)
+    {
+        if(decimal.TryParse(newValue, out decimal price))
+        {
+            Price = price;
+        }
+    }
+
+    [ObservableProperty]
+    [Required(ErrorMessage = "اسم المنتج مطلوب")]
+    [MinLength(2, ErrorMessage = "الاسم يجب أن يكون أكثر من 2 حرف")]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+    private string _name;
+
+    [ObservableProperty]
+    [Required(ErrorMessage = "القسم مطلوب")]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+    private CategoryInfoDto _selectedCategory;
+
+    partial void OnSelectedCategoryChanged(CategoryInfoDto oldValue, CategoryInfoDto newValue)
+    {
+        CategorySelection.SelectedCategory = newValue;
+    }
+
+    [ObservableProperty]
+    [Required(ErrorMessage = "السعر مطلوب")]
+    [RegularExpression(@"^\d+([.,]\d{0,2})?$", ErrorMessage = "السعر غير صالح")]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+    private string _priceText;
+
+    [ObservableProperty]
+    [Required(ErrorMessage = "كود المنتج مطلوب")]
+    [RegularExpression(@"^\d{3}-\d{2}-\d{2}", ErrorMessage = "كود المنتج يتكون من الصورة 000-00-00")]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+    private string _code;
+
+    public decimal Price { get; private set; }
+
+    [ObservableProperty]
+    private IEnumerable<SelectableObject<SupplierInfoDto>> _suppliers;
+
+    [ObservableProperty]
+    private IEnumerable<CategoryInfoDto> _categories;
+
+    public IEnumerable<PropertyItem> Properties { get; private set; }
+    public IEnumerable<EnumItem<PropertyRequirement>> PropertyRequirements { get; private set; } 
+}
+
+public partial class PropertyItem : ObservableObject
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string PropertyType { get; set; }
+
+    [ObservableProperty]
+    private EnumItem<PropertyRequirement> _propertyRequirement;
+
+    [ObservableProperty]
+    private bool _isAssigned;
+
+}
+
+public enum PropertyRequirement
+{
+    [Description("اختيارية")]
+    Optional,
+
+    [Description("مطلوبة")]
+    Required
 }
