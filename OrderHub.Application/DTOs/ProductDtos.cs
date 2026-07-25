b6e50ef7eb9b415f9d1003d4c267e1412681bdd5
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2026-07-25 22:01:31 +0300

    chore: wrap parameters

--- OrderHub.Application/DTOs/ProductDtos.cs
+++ OrderHub.Application/DTOs/ProductDtos.cs
@@ -5,6 +5,19 @@ namespace OrderHub.Application.DTOs;
 
 public class ProductDtos
 {
-    public record ProductListDto(int Id, string Name, decimal Price, string Code, string CategoryName, IReadOnlyCollection<int> SupplierIds, IReadOnlyCollection<SupplierInfoDto> Suppliers);
-    public record ProductFormDto(string Name, string Code, decimal Price, int CategoryId, IEnumerable<int> SelectedSuppliersIds);
+    public record ProductListDto(
+        int Id,
+        string Name,
+        decimal Price,
+        string Code,
+        string CategoryName,
+        IReadOnlyCollection<int> SupplierIds,
+        IReadOnlyCollection<SupplierInfoDto> Suppliers);
+
+    public record ProductFormDto(
+        string Name,
+        string Code,
+        decimal Price,
+        int CategoryId,
+        IEnumerable<int> SelectedSuppliersIds);
 }
